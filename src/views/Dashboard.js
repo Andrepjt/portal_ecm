c167d7b49e89bbac2d44294b6d63ebe3d5416192
hafizrahmadi <hafiz.rahmadi@gmail.com>
2020-02-20 15:13:45 +0700

    Menambahkan opacity 0.9 pada dashboard.js

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -176,7 +176,7 @@ class Dashboard extends Component{
           <div className="px-4 md:px-10 mx-auto w-full -m-24">
             <div className="flex flex-wrap">
               <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-gray-900">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-gray-900" style={{opacity:0.9}}>
                   <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
                     <div className="flex flex-wrap items-center">
                       <div className="relative w-full max-w-full flex-grow flex-1">
@@ -254,7 +254,7 @@ class Dashboard extends Component{
               </div>
 
                 <div className="w-full xl:w-4/12 px-4">
-                  <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                  <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded" style={{opacity:0.9}}>
                     <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
                       <div className="flex flex-wrap items-center">
                         <div className="relative w-full max-w-full flex-grow flex-1">
